42a2f7567c02d7ca7811812f02b9326817a8878c
Maxime1619 <81220757+Maxime1619@users.noreply.github.com>
2022-10-23 17:25:29 +0200

    Le plus a jour

---
+++ FPChrono.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <time.h>
+
+int main (void) {
+	
+	time_t start = time (NULL); 
+	
+	printf("appuyer sur une touche pour arrêter le chrono et afficher le temps\n"); 
+	
+   	getchar();
+	
+	printf("duree = %ds\n", (int) (time(NULL)-start)); 
+	
+	sleep(3); 
+	
+    exit(0);
+	
+}
+
+
